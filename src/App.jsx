df503476c070e2d60e4d1db06039e18528ea4f13
HuyenBanhBao <ngoctung2307th@gmail.com>
2025-05-13 10:54:40 +0700

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { useColorScheme } from "@mui/material";
-
-function ModeToggle() {
-    const { mode, setMode } = useColorScheme();
-    return (
-        <Button
-            onClick={() => {
-                setMode(mode === "dark" ? "light" : "dark");
-            }}
-        >
-            {mode === "light" ? "Turn dark" : "Turn light"}
-        </Button>
-    );
-}
-
+// --------------------- IMPORT COMPONENTS -------------------------
+import Board from "./pages/Boards/_id";
+// --------------------- FUNCTIONAL COMPONENTS ---------------------
+// --------------------- MAIN COMPONENT ---------------------
 function App() {
     return (
         <>
-            <ModeToggle />
-            <hr />
-            <Typography variant="h4" component="div">
-                Hello world
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                Hello world
-            </Typography>
-
-            <Stack spacing={2} direction="row">
-                <Button variant="text">Text</Button>
-                <Button variant="contained">Contained</Button>
-                <Button variant="outlined">Outlined</Button>
-            </Stack>
+            {/* React router dom */}
+            <Board />
         </>
     );
 }
---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,23 @@
+// --------------------- IMPORT FROM LIBRATORY ---------------------
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+// --------------------- IMPORT COMPONENTS ---------------------
+import AppBar from "../../components/AppBar";
+import BoardBar from "./BoardBar";
+import BoardContent from "./BoardContent";
+// --------------------- MAIN COMPONENT ---------------------
+const Board = () => {
+    return (
+        <>
+            <Container disableGutters maxWidth={false} sx={{ height: "100vh", backgroundColor: "primary.main" }}>
+                <AppBar />
+                <BoardBar />
+                <BoardContent />
+            </Container>
+        </>
+    );
+};
+
+export default Board;
