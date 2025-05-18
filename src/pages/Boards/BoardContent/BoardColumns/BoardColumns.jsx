f8e8b74504373b48e1744b54ce0c5771869d337d
HuyenBanhBao <ngoctung2307th@gmail.com>
2025-05-18 09:41:35 +0700

    Add DND-KIT to Project

--- src/pages/Boards/BoardContent/BoardColumns/BoardColumns.jsx
+++ src/pages/Boards/BoardContent/BoardColumns/BoardColumns.jsx
@@ -3,51 +3,54 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import BoardColumn from "./BoardColumn/BoardColumn";
 import LibraryAddIcon from "@mui/icons-material/LibraryAdd";
-
+// --------------------- DND KIT ---------------------
+import { SortableContext, horizontalListSortingStrategy } from "@dnd-kit/sortable";
 // ---------------------------------- MAIN COMPONENT ---------------------
 const BoardColumns = ({ columns }) => {
     return (
         <>
-            <Box
-                sx={{
-                    background: "inherit",
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    overflowX: "auto",
-                    overflowY: "hidden",
-                    "&::-webkit-scrollbar-track": { m: 2 },
-                }}
-            >
-                {columns?.map((column) => (
-                    <BoardColumn key={column._id} column={column} />
-                ))}
-
-                {/* -------------------- ADD NEW COLUMN -------------------- */}
+            <SortableContext items={columns?.map((c) => c._id)} strategy={horizontalListSortingStrategy}>
                 <Box
                     sx={{
-                        minWidth: "300px",
-                        maxWidth: "300px",
-                        bgcolor: "#ffffff3d",
-                        ml: 2,
-                        height: "fit-content",
-                        borderRadius: "6px",
+                        background: "inherit",
+                        width: "100%",
+                        height: "100%",
+                        display: "flex",
+                        overflowX: "auto",
+                        overflowY: "hidden",
+                        "&::-webkit-scrollbar-track": { m: 2 },
                     }}
                 >
-                    <Button
-                        startIcon={<LibraryAddIcon />}
+                    {columns?.map((column) => (
+                        <BoardColumn key={column._id} column={column} />
+                    ))}
+
+                    {/* -------------------- ADD NEW COLUMN -------------------- */}
+                    <Box
                         sx={{
-                            color: (theme) => theme.trello.primaryColorTextBar,
-                            p: 1,
+                            minWidth: "300px",
+                            maxWidth: "300px",
+                            bgcolor: "#ffffff3d",
                             ml: 2,
-                            width: "100%",
-                            justifyContent: "flex-start",
+                            height: "fit-content",
+                            borderRadius: "6px",
                         }}
                     >
-                        Add new column
-                    </Button>
+                        <Button
+                            startIcon={<LibraryAddIcon />}
+                            sx={{
+                                color: (theme) => theme.trello.primaryColorTextBar,
+                                p: 1,
+                                ml: 2,
+                                width: "100%",
+                                justifyContent: "flex-start",
+                            }}
+                        >
+                            Add new column
+                        </Button>
+                    </Box>
                 </Box>
-            </Box>
+            </SortableContext>
         </>
     );
 };
--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,31 +1,75 @@
 // ---------------------- IMPORT LIB ----------------------
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 // --------------------- IMPORT COMPONENTS ---------------------
 import BoardColumns from "./BoardColumns/BoardColumns";
 // -------------------- IMPORT UTILS ---------------------
 import { mapOrder } from "~/utils/sorts";
+// --------------------- DND KIT ---------------------
+import { DndContext, PointerSensor, useSensors, useSensor, MouseSensor, TouchSensor } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+// ----------------------------------------------------------
 // --------------------- MAIN COMPONENT ---------------------
 const BoardContent = ({ board }) => {
+    const [orderedColumns, setOrderedColumns] = useState([]);
     // --------------------- FUNCTION ---------------------
-    const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, "_id");
 
+    // https://docs.dndkit.com/api-documentation/sensors
+    // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } });
+    // Yêu cầu chuột di chuyển ít nhất 10px thì mới gọi event. Tránh trường hợp gọi hàm khi click chuột vào column
+    const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } });
+    // Nhấn giữ 250ms và dung sai của cảm ừng thì mới kích hoạt event
+    const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 200, tolerance: 500 } });
+
+    // const mySensors = useSensors(pointerSensor);
+    const mySensors = useSensors(mouseSensor, touchSensor);
+    //
+    useEffect(() => {
+        setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
+    }, [board]);
+
+    // --------------------- Function handle drag end ---------------------
+    const handleDragEnd = (event) => {
+        console.log(event);
+
+        const { active, over } = event;
+
+        // check: if active and over are null -> return
+        if (!active || !over) return;
+
+        // check: if active and over are not equal -> return
+        if (active.id !== over.id) {
+            //
+            const oldIndex = orderedColumns.findIndex((c) => c._id === active.id);
+            // index of the column to be moved (Vị trí của phần tử cần di chuyển)
+            const newIndex = orderedColumns.findIndex((c) => c._id === over.id);
+            // index of the column to be moved to (Vị trí đích nơi phần tử sẽ được chuyển đến)
+            const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex);
+            // const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id);
+            // console.log("dndOrderedColumnsIds: ", dndOrderedColumnsIds);
+            // console.log("dndOrderedColumns: ", dndOrderedColumns);
+
+            setOrderedColumns(dndOrderedColumns);
+        }
+    };
     // --------------------- RETURN ---------------------
     return (
         <>
-            <Box
-                sx={{
-                    width: "100%",
-                    p: "10px 0",
-                    height: (theme) => theme.trello.boardContentHeight,
-                    background: (theme) =>
-                        theme.palette.mode === "dark" ? theme.trello.gradientBgDark : theme.trello.gradientBg,
-                }}
-            >
-                {/* --------------------- BOX COLUMNS ---------------------- */}
-                {/* <BoardColumns columns={board?.columns} /> */}
-                <BoardColumns columns={orderedColumns} />
-            </Box>
+            <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+                <Box
+                    sx={{
+                        width: "100%",
+                        p: "10px 0",
+                        height: (theme) => theme.trello.boardContentHeight,
+                        background: (theme) =>
+                            theme.palette.mode === "dark" ? theme.trello.gradientBgDark : theme.trello.gradientBg,
+                    }}
+                >
+                    {/* --------------------- BOX COLUMNS ---------------------- */}
+                    {/* <BoardColumns columns={board?.columns} /> */}
+                    <BoardColumns columns={orderedColumns} />
+                </Box>
+            </DndContext>
         </>
     );
 };
